ea73e45da9c948d9973335560c92a93fdc7c06be
fy15912250653 <34825140+fy15912250653@users.noreply.github.com>
2019-10-13 09:50:29 +0800

    cloud之eureka集群配置

---
+++ cloud-eureka-7003/src/main/java/com/fy/cloud/DeptEurekaApplication7003.java
@@ -0,0 +1,19 @@
+package com.fy.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * EnableEurekaServer EurekaServer服务器端启动类,接受其它微服务注册进来
+ *
+ * @author fy
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DeptEurekaApplication7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptEurekaApplication7003.class, args);
+
+    }
+}
--- cloud-provider-dept-8001/src/main/java/com/fy/cloud/DeptProviderApplication8001.java
+++ cloud-provider-dept-8001/src/main/java/com/fy/cloud/DeptProviderApplication8001.java
@@ -2,13 +2,16 @@ package com.fy.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author fy
+ * EnableEurekaClient本服务启动后会自动注册进eureka服务中
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProviderApplication8001 {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProviderApplication8001.class,args);
+        SpringApplication.run(DeptProviderApplication8001.class, args);
     }
 }
