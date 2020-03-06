5837a919624114ee00c48163d44d291dd7bcebe0
fy15912250653 <34825140+fy15912250653@users.noreply.github.com>
2020-03-06 14:25:35 +0800

    ribbon自定义负载均衡

--- cloud-consumer-dept-80/src/main/java/com/fy/cloud/DeptConsumerApplication80.java
+++ cloud-consumer-dept-80/src/main/java/com/fy/cloud/DeptConsumerApplication80.java
@@ -1,14 +1,18 @@
 package com.fy.cloud;
 
+import com.fy.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * @author fy
  */
 @SpringBootApplication
 @EnableEurekaClient
+// @RibbonClient导入的rule类不能在@ComponentScans()扫描的包下
+@RibbonClient(name="CLOUD-DEPT",configuration= MySelfRule.class)
 public class DeptConsumerApplication80 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumerApplication80.class,args);
